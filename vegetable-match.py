f24a211a083f9b365120bfb222e76b66683d1539
jonas <65377489+artofjonas@users.noreply.github.com>
2026-07-08 08:34:50 -0700

    Add commentary

--- vegetable-match.py
+++ vegetable-match.py
@@ -1,8 +1,10 @@
 # Veggie list provided courtesy of https://copypastelist.com/food/list/list-of-vegetables
 
+# Open and parse vegetable list
 with open("./vegetable_list.txt", "r", encoding="utf-8") as f:
     vegetable_list = [line.strip() for line in f.readlines()]
 
+# search for a vegetable
 vegetable = input("Enter a vegetable: ")
 match vegetable:
     case _ if vegetable in vegetable_list:
