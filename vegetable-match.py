2c108d861f69cc7b9b1309f300c54c21f0350963
jonas <jonas.silver@gmail.com>
2026-07-09 09:51:06 -0700

    Add next goal comment

--- vegetable-match.py
+++ vegetable-match.py
@@ -1,4 +1,5 @@
 # Veggie list provided courtesy of https://copypastelist.com/food/list/list-of-vegetables
+# to do: add some sort of try-except loop to handle bogus input
 
 # Open and parse vegetable list
 with open("./vegetable_list.txt", "r", encoding="utf-8") as f:
