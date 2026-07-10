dfc178c7dc24a25900e959a9ecaa33924710ec14
jonas <jonas.silver@gmail.com>
2026-07-10 11:24:42 -0700

    Alter to-do list

--- vegetable-match.py
+++ vegetable-match.py
@@ -1,5 +1,6 @@
 # Veggie list provided courtesy of https://copypastelist.com/food/list/list-of-vegetables
 # to do: add a try-except FileNotFoundError in the event that the vegetable_list.txt file is not found or badly formatted
+# to do: add a multiple results function in the event of a partial match, e.g. "pea" returns "pea", "snap pea", "snow pea", etc.
 
 # Open and parse vegetable list
 with open("./vegetable_list.txt", "r", encoding="utf-8") as f:
